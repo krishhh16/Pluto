494ce1d2048b3870d034e9f71964ff7c48ece7cd
Zeref <krishh16@yahoo.com>
2024-11-01 20:10:20 +0530

    added a test to check if the swap functionality works :

--- pluto-program/tests/swap.ts
+++ pluto-program/tests/swap.ts
@@ -2,7 +2,7 @@ import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { PlutoProgram } from "../target/types/pluto_program";
 import { getValues, mintingTokens } from "../utils"
-import { ASSOCIATED_TOKEN_PROGRAM_ID, TOKEN_PROGRAM_ID, getAccount, getMint } from "@solana/spl-token";
+import { ASSOCIATED_TOKEN_PROGRAM_ID, TOKEN_PROGRAM_ID, createAssociatedTokenAccountInstruction, getAccount, getMint } from "@solana/spl-token";
 import { Keypair, PublicKey } from "@solana/web3.js"
 import { SYSTEM_PROGRAM_ID } from "@coral-xyz/anchor/dist/cjs/native/system";
 import { assert } from "chai";
@@ -34,11 +34,11 @@ describe("Withdraw", () => {
     const provider = anchor.AnchorProvider.env();
     anchor.setProvider(provider);
     console.log("This is my create_pool test")
-    
-    const MINIMUM_LIQUIDITY = 100;
-    const liquidity = Math.sqrt(Amounts.amount_a * Amounts.amount_b);
-    const program = anchor.workspace.PlutoProgram as Program<PlutoProgram>;
 
+    const MINIMUM_LIQUIDITY = 100;
+    const program = anchor.workspace.PlutoProgram as Program<PlutoProgram>;
+    const swapAmountA = new anchor.BN(Amounts.amount_a - 50)
+    const swapAmountB = new anchor.BN(Amounts.amount_a - 50)
 
     let vals: GetValuesReturnType;
     beforeEach(async () => {
@@ -94,42 +94,87 @@ describe("Withdraw", () => {
     })
 
     it("Swaps A", async () => {
-        await program.methods.swapTokens(true, new anchor.BN(Amounts.amount_a - 50), new anchor.BN(50))
-        .accountsStrict({
-            associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
-            liquidityPool: vals.liquidityPool,
-            mintA: vals.mintAKeypair.publicKey,
-            mintB: vals.mintBKeypair.publicKey,
-            mintLiquidity: vals.mintToken,
-            poolAccountA: vals.poolAccountA,
-            poolAccountB: vals.poolAccountB,
-            poolAuthority: vals.poolAuthority,
-            systemProgram: SYSTEM_PROGRAM_ID,
-            tokenProgram: TOKEN_PROGRAM_ID,
-            trader: vals.payer.publicKey,
-            traderAtaA: vals.holderAccountA,
-            traderAtaB: vals.holderAccountB
-        })
-        .signers([vals.payer])
+        await program.methods.swapTokens(true, swapAmountA, new anchor.BN(50))
+            .accountsStrict({
+                associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
+                liquidityPool: vals.liquidityPool,
+                mintA: vals.mintAKeypair.publicKey,
+                mintB: vals.mintBKeypair.publicKey,
+                mintLiquidity: vals.mintToken,
+                poolAccountA: vals.poolAccountA,
+                poolAccountB: vals.poolAccountB,
+                poolAuthority: vals.poolAuthority,
+                systemProgram: SYSTEM_PROGRAM_ID,
+                tokenProgram: TOKEN_PROGRAM_ID,
+                trader: vals.payer.publicKey,
+                traderAtaA: vals.holderAccountA,
+                traderAtaB: vals.holderAccountB
+            })
+            .signers([vals.payer])
     })
     it("Swaps B", async () => {
-        await program.methods.swapTokens(false, new anchor.BN(Amounts.amount_a - 50), new anchor.BN(50))
-        .accountsStrict({
-            associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
-            liquidityPool: vals.liquidityPool,
-            mintA: vals.mintAKeypair.publicKey,
-            mintB: vals.mintBKeypair.publicKey,
-            mintLiquidity: vals.mintToken,
-            poolAccountA: vals.poolAccountA,
-            poolAccountB: vals.poolAccountB,
-            poolAuthority: vals.poolAuthority,
-            systemProgram: SYSTEM_PROGRAM_ID,
-            tokenProgram: TOKEN_PROGRAM_ID,
-            trader: vals.payer.publicKey,
-            traderAtaA: vals.holderAccountA,
-            traderAtaB: vals.holderAccountB
-        })
-        .signers([vals.payer])
+        await program.methods.swapTokens(false, swapAmountB, new anchor.BN(50))
+            .accountsStrict({
+                associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
+                liquidityPool: vals.liquidityPool,
+                mintA: vals.mintAKeypair.publicKey,
+                mintB: vals.mintBKeypair.publicKey,
+                mintLiquidity: vals.mintToken,
+                poolAccountA: vals.poolAccountA,
+                poolAccountB: vals.poolAccountB,
+                poolAuthority: vals.poolAuthority,
+                systemProgram: SYSTEM_PROGRAM_ID,
+                tokenProgram: TOKEN_PROGRAM_ID,
+                trader: vals.payer.publicKey,
+                traderAtaA: vals.holderAccountA,
+                traderAtaB: vals.holderAccountB
+            })
+            .signers([vals.payer])
+    })
+
+    it("confirms if the swap A amount was diduced from the pool", async () => {
+
+        const poolAccountABefore = await getAccount(provider.connection, vals.poolAccountA);
+        const poolAccountBBefore = await getAccount(provider.connection, vals.poolAccountB);
+        const traderAccountABefore = await getAccount(provider.connection, vals.holderAccountA);
+        const traderAccountBBerfore = await getAccount(provider.connection, vals.holderAccountB);
+
+        await program.methods.swapTokens(true, swapAmountA, new anchor.BN(50))
+            .accountsStrict({
+                associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
+                liquidityPool: vals.liquidityPool,
+                mintA: vals.mintAKeypair.publicKey,
+                mintB: vals.mintBKeypair.publicKey,
+                mintLiquidity: vals.mintToken,
+                poolAccountA: vals.poolAccountA,
+                poolAccountB: vals.poolAccountB,
+                poolAuthority: vals.poolAuthority,
+                systemProgram: SYSTEM_PROGRAM_ID,
+                tokenProgram: TOKEN_PROGRAM_ID,
+                trader: vals.payer.publicKey,
+                traderAtaA: vals.holderAccountA,
+                traderAtaB: vals.holderAccountB
+            })
+            .signers([vals.payer])
+
+        const poolAccountAAfter = await getAccount(provider.connection, vals.poolAccountA);
+        const poolAccountBAfter = await getAccount(provider.connection, vals.poolAccountB);
+        const traderAccountAAfter = await getAccount(provider.connection, vals.holderAccountA);
+        const traderAccountBAfter = await getAccount(provider.connection, vals.holderAccountB);
+        
+        
+        const taxedAmount = new BN(swapAmountA).sub(new BN(swapAmountA.mul(new BN(500)).div(new BN(10000))))
+        
+        const outputA = new BN(taxedAmount.mul(new BN(poolAccountBBefore.amount.toString()))).div(new BN(
+            new BN(poolAccountABefore.amount.toString()).add(taxedAmount)
+        ))
+
+        assert(
+            new BN(traderAccountAAfter.amount.toString()).eq(
+                new BN(traderAccountABefore.amount.toString()).sub(outputA)
+            ),
+            "The output amount should have been deducted from the trader's ATA"
+        )
     })
 
 })
